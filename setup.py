c05511db51ba8a7b4f873205fe66e5589e5fa9d2
mzguntalan <mzguntalan@gmail.com>
2025-08-22 16:39:08 +0900

    released 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="z-zytome",
-    version="0.0.3",
+    version="0.0.4",
     description="Zytome is declarative library for handling gene expression data for various data portals or datasets.",
     url="https://github.com/mzguntalan/zytome",
     author="Marko Zolo Gozano Untalan",
--- zytome/__init__.py
+++ zytome/__init__.py
@@ -1,4 +1,4 @@
 """zytome"""
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Marko Zolo Gozano Untalan"
